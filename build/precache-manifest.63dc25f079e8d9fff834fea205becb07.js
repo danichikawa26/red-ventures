f723eeaa206ce8f366218b5086a561a21fe16588
Daniela Ichikawa <danichikawa@gmail.com>
2019-10-08 21:36:46 -0300

    remove yark.lock

---
+++ build/precache-manifest.63dc25f079e8d9fff834fea205becb07.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9be82b0cd7979854107b5857a8cabf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a62ac02a250b3d71770",
+    "url": "/static/css/main.d920a5ad.chunk.css"
+  },
+  {
+    "revision": "f63e41ade0843e90d34c",
+    "url": "/static/js/2.b6963c81.chunk.js"
+  },
+  {
+    "revision": "2a62ac02a250b3d71770",
+    "url": "/static/js/main.40840e5e.chunk.js"
+  },
+  {
+    "revision": "9d264f947be657e5b3a4",
+    "url": "/static/js/runtime-main.47cda44d.js"
+  },
+  {
+    "revision": "5482e0a0b5b98e8eeda44d36457982b3",
+    "url": "/static/media/1.5482e0a0.png"
+  },
+  {
+    "revision": "9ce7e2e85ebab17fc8ba8ca51ca6f35f",
+    "url": "/static/media/2.9ce7e2e8.png"
+  },
+  {
+    "revision": "9ce7e2e85ebab17fc8ba8ca51ca6f35f",
+    "url": "/static/media/3.9ce7e2e8.png"
+  },
+  {
+    "revision": "c59f3311700141ea7c8aefa775f0bcdc",
+    "url": "/static/media/4.c59f3311.png"
+  },
+  {
+    "revision": "dc530f4a7154f95eed85d57ceaf329b4",
+    "url": "/static/media/5.dc530f4a.png"
+  },
+  {
+    "revision": "8a836847b0a0139adc7dd4a63c997e3e",
+    "url": "/static/media/6.8a836847.png"
+  },
+  {
+    "revision": "7864a3a18ed150d75782e1d36dfe11d6",
+    "url": "/static/media/7.7864a3a1.png"
+  },
+  {
+    "revision": "f4004546a058a36c18fe35e02cfce1a2",
+    "url": "/static/media/8.f4004546.png"
+  },
+  {
+    "revision": "3097a2dad1972e659c4bc8497594373a",
+    "url": "/static/media/9.3097a2da.png"
+  },
+  {
+    "revision": "3ee26ef04eb3a1e321cb74102d13284e",
+    "url": "/static/media/RV-logo.3ee26ef0.svg"
+  },
+  {
+    "revision": "d2ef5a0d79f9d5d9bed714097374786d",
+    "url": "/static/media/arrow.d2ef5a0d.svg"
+  },
+  {
+    "revision": "63d87a1ac7a6062585c1ffae60f036b0",
+    "url": "/static/media/car-home.63d87a1a.png"
+  },
+  {
+    "revision": "dae92a59509299532749d82e633875e6",
+    "url": "/static/media/final-4.dae92a59.png"
+  },
+  {
+    "revision": "c2f9aa82e67f7eb96047ebd19fed4a65",
+    "url": "/static/media/final-5.c2f9aa82.png"
+  },
+  {
+    "revision": "2e84478b2c653d90168b2a0fd423f5a3",
+    "url": "/static/media/final-6.2e84478b.png"
+  },
+  {
+    "revision": "3eb12b35065a99cb723dc9498804e8b0",
+    "url": "/static/media/rebuild.3eb12b35.svg"
+  }
+]);
